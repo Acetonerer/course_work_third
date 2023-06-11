5f419ea849141ae366577464af732c55d77bcd72
Acetonerer <bolokan2014@gmail.com>
2023-06-11 21:56:31 +0700

    Дописаны тесты к load_data

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 from utils.func import load_data, form_card_account, form_date
 
-FILE = "info.json"
+FILE = "oper_test.json"
 
 
 def test_form_date():
@@ -16,3 +16,32 @@ def test_form_card_account():
     assert form_card_account("Maestro 3806652527413662") == "Maestro 3806 65 ** **** 3662"
 
 
+def test_load_data():
+    file = [
+            {
+                "id": 736942989,
+                "state": "EXECUTED",
+                "date": "2019-09-06T00:48:01.081967"
+            },
+            {
+                "id": 580054042,
+                "state": "EXECUTED",
+                "date": "2018-06-20T03:59:34.851630"
+            },
+            {
+                "id": 619287771,
+                "state": "EXECUTED",
+                "date": "2019-08-19T16:30:41.967497"
+            },
+            {
+                "id": 179194306,
+                "state": "EXECUTED",
+                "date": "2019-05-19T12:51:49.023880"
+            },
+            {
+                "id": 921286598,
+                "state": "EXECUTED",
+                "date": "2018-03-09T23:57:37.537412"
+            }
+    ]
+    assert load_data(FILE) != file
