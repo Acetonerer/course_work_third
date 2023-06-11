725664f77f537f485ce16c0dd3a3fd7b19ad3f71
Acetonerer <bolokan2014@gmail.com>
2023-06-11 21:38:43 +0700

    Написан основной код в файле main.py

---
+++ utils/main.py
@@ -0,0 +1,22 @@
+from utils.func import load_data, form_card_account, form_date
+
+FILE = "operations.json"
+
+
+def main():
+    operations = load_data(FILE)
+    for operation in operations:
+        operation["date"] = form_date(operation["date"])
+        try:
+            operation["from"] = form_card_account(operation["from"])
+        except LookupError:
+            operation["from"] = "Неизвестный источник"
+        operation["to"] = form_card_account(operation["to"])
+        print(operation["date"] + " " + operation["description"])
+        print(operation["from"] + " -> " + operation["to"])
+        print(operation["operationAmount"]["amount"] + " "
+              + operation["operationAmount"]["currency"]["name"], end="\n\n")
+
+
+if __name__ == "__main__":
+    main()
